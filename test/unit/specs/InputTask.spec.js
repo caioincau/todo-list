cb52d7d5ea91ced9aa3149400056ab916508d633
Caio Incau <caioincau@gmail.com>
2017-02-16 21:28:03 -0200

    spec

--- test/unit/specs/InputTask.spec.js
+++ test/unit/specs/InputTask.spec.js
@@ -3,9 +3,8 @@ import InputTask from 'src/components/InputTask'
 
 describe('InputTask.vue', () => {
   it('should render correct contents', () => {
-    const Ctor = Vue.extend(InputTask)
-    const vm = new Ctor().$mount()
-    console.log(vm.$el.querySelectorAll('.new-todo'))
+    const Constructor = Vue.extend(InputTask)
+    const vm = new Constructor().$mount()
     expect(vm.$el.querySelectorAll('.new-todo').length).to.equal(1)
   })
 })
