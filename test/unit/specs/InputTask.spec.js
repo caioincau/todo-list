f5617d3a715a339e32ff3301b62c4c0d48815827
Caio Incau <caioincau@gmail.com>
2017-02-17 21:48:03 -0200

    better spec

--- test/unit/specs/InputTask.spec.js
+++ test/unit/specs/InputTask.spec.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import InputTask from 'src/components/InputTask'
 
+let construct = () => {
+  const Constructor = Vue.extend(InputTask)
+  const vm = new Constructor().$mount()
+  return vm
+}
+
 describe('InputTask.vue', () => {
-  it('should render correct contents', () => {
+
+  let vm = {}
+
+  beforeEach(function() {
     const Constructor = Vue.extend(InputTask)
-    const vm = new Constructor().$mount()
+    vm = new Constructor().$mount()
+  });
+
+  it('should render correct contents', () => {
     expect(vm.$el.querySelectorAll('.new-todo').length).to.equal(1)
   })
   it('should create the task correctly', () => {
-    const Constructor = Vue.extend(InputTask)
-    const vm = new Constructor().$mount()
     let task = vm.createTask('Comprar leite')
     expect(task.title).to.equal('Comprar leite')
+  })
+  it('should create the task not completed', () => {
+    let task = vm.createTask('Comprar leite')
+    expect(task.completed).to.be.false
+  })
+  it('should clean the input', () => {
+    vm.$el.querySelector('.new-todo').value = 'Comprar Leite'
+    vm.clearField()
+    expect(vm.$el.querySelector('.new-todo').value).to.equal('')
   })
 })
